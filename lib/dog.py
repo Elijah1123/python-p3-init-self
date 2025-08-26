17bed9beb30adb02b99af0fe7580751d257d8176
Elijah1123 <elijahmzalendo659@gmail.com>
2025-08-26 12:06:15 +0300

    python

--- lib/dog.py
+++ lib/dog.py
@@ -4,9 +4,16 @@ class Dog:
     def __init__(self, name, breed="Mutt"):  # Default breed is set to "Mutt"
         self.name = name
         self.breed = breed
-
+    # print(f"The {name} is a  {breed} Dog")
     def bark(self):
         print("Woof!")
 
     def sit(self):
         print("The dog is sitting.")
+
+
+Tom = Dog("Tom")
+Tom.name()
+
+# German Shepherd = Dog("German Shepherd")
+# German Shepherd.breed()
--- lib/person.py
+++ lib/person.py
@@ -3,10 +3,17 @@
 class Person:
     def __init__(self, name):  # Accepts a name argument
         self.name = name
+        print(f"{name} is born!")
 
     def talk(self):
-        print("Hello World!")
+        print("Good Morning {name}?")
 
     def walk(self):
         print("The person is walking.")
 
+
+Mzalendo = Person("Mzalendo")
+Zakayo = Person("Zakayo")
+Pabro = Person("Pabro")
+Lyn = Person("Lyn")
+Davi = Person("Davi")
